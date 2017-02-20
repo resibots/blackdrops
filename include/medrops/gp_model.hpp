7f69bff06fe656336bad50520496d046c05e2be9
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2017-02-20 16:47:19 +0100

    Small typo

--- include/medrops/gp_model.hpp
+++ include/medrops/gp_model.hpp
@@ -116,10 +116,7 @@ namespace medrops {
         void save_data(const std::string& filename) const
         {
             const std::vector<Eigen::VectorXd>& samples = _gp_models[0]->samples();
-            Eigen::MatrixXd observations(samples.size(), _gp_models.size());
-            for (size_t i = 0; i < _gp_models.size(); ++i) {
-                observations.col(i) = _gp_models[i]->observations().col(0);
-            }
+            Eigen::MatrixXd observations = _observations;
 
             std::ofstream ofs_data(filename);
             for (size_t i = 0; i < samples.size(); ++i) {
