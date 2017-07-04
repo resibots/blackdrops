e7af761fbc156c0830a4d8c3e33fde58fdc72632
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2017-07-04 16:01:46 +0200

    Fixes in scripts

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cwd=$(pwd)
 
+# go to limbo directory
+cd limbo
+
 # save current directory
 ./waf --libcmaes=${cwd}/install --dart=${cwd}/install --robot_dart=${cwd}/install --exp blackdrops
+
+# go back to original directory
+cd ..
--- scripts/configure.sh
+++ scripts/configure.sh
@@ -6,8 +6,11 @@ cd limbo
 mkdir -p exp
 cd exp
 ln -s ../../ blackdrops
-# go back to original directory
-cd ../..
+# go back to limbo
+cd ..
 
 # save current directory
 ./waf configure --libcmaes=${cwd}/install --dart=${cwd}/install --robot_dart=${cwd}/install --exp blackdrops
+
+# go back to original directory
+cd ..
