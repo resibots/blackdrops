15a4f43204a0a6c53dcb9e3df4ca69972de68188
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2017-07-19 13:01:06 +0200

    improve scripts

--- scripts/install_deps_req.sh
+++ scripts/install_deps_req.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-sudo apt-get -qq update
-# install Eigen 3, Boost and TBB
-sudo apt-get --yes --force-yes install cmake libeigen3-dev libboost-serialization-dev libboost-filesystem-dev libboost-test-dev libboost-program-options-dev libboost-thread-dev libboost-regex-dev
-# install google tests for libcmaes
-sudo apt-get --yes --force-yes install libgtest-dev autoconf automake libtool libgoogle-glog-dev libgflags-dev
+OS=$(uname)
+echo "Detected OS: $OS"
+
+if [ $OS = "Darwin" ]; then
+    echo "Installing for OSX via brew"
+    brew install cmake eigen3 boost
+    brew install autoconf automake
+else
+    echo "Installing for Ubuntun via apt-get"
+    sudo apt-get -qq update
+    # install Eigen 3, Boost and TBB
+    sudo apt-get --yes --force-yes install cmake libeigen3-dev libboost-serialization-dev libboost-filesystem-dev libboost-test-dev libboost-program-options-dev libboost-thread-dev libboost-regex-dev
+    # install google tests for libcmaes
+    sudo apt-get --yes --force-yes install libgtest-dev autoconf automake libtool libgoogle-glog-dev libgflags-dev
+fi
 
 # save current directory
 cwd=$(pwd)
