06b170d99c0233cd50a7591d5ba815d2d925f1bc
Rituraj Kaushik <rituraj.kaushik95@gmail.com>
2017-07-20 16:36:21 +0200

    Adds double pendulum scenario

---
+++ src/classic_control/double_pendulum.cpp
@@ -0,0 +1,424 @@
+#include <limbo/limbo.hpp>
+
+#include <boost/numeric/odeint.hpp>
+#include <boost/program_options.hpp>
+
+#include <blackdrops/blackdrops.hpp>
+#include <blackdrops/gp_model.hpp>
+#include <blackdrops/model/gp/kernel_lf_opt.hpp>
+#include <blackdrops/model/multi_gp.hpp>
+#include <blackdrops/model/multi_gp/multi_gp_parallel_opt.hpp>
+#include <blackdrops/system/ode_system.hpp>
+
+#include <blackdrops/policy/nn_policy.hpp>
+
+#include <utils/utils.hpp>
+
+#if defined(USE_SDL) && !defined(NODSP)
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//The window we'll be rendering to
+SDL_Window* window = NULL;
+
+//The window renderer
+SDL_Renderer* renderer = NULL;
+
+bool sdl_init()
+{
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    window = SDL_CreateWindow("Double Pendulum Task", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    //Create renderer for window
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        std::cout << "Renderer could not be created! SDL Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    //Initialize renderer color
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+    //Update the surface
+    SDL_UpdateWindowSurface(window);
+
+    //If everything initialized fine
+    return true;
+}
+
+bool draw_pendulum(double theta1, double theta2, bool red = false)
+{
+    double l = 0.5;
+    // double c1 = std::cos(theta1), s1 = std::sin(theta1);
+    // double c12 = std::cos(theta1 + theta2), s12 = std::sin(theta1 + theta2);
+
+    double x1 = -l * std::sin(theta1), y1 = l * std::cos(theta1);
+    double x2 = -l*(std::sin(theta1)+std::sin(theta2)) , y2 = l*(std::cos(theta1)+std::cos(theta2));
+
+    //Draw blue horizontal line
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF);
+    if (red)
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+    SDL_Rect outlineRect = {static_cast<int>(SCREEN_WIDTH / 2 - 0.05 * SCREEN_HEIGHT / 4), static_cast<int>(SCREEN_HEIGHT / 2 - 0.05 * SCREEN_HEIGHT / 4), static_cast<int>(0.1 * SCREEN_HEIGHT / 4), static_cast<int>(0.1 * SCREEN_HEIGHT / 4)};
+    SDL_RenderFillRect(renderer, &outlineRect);
+    outlineRect = {SCREEN_WIDTH / 2 + x1 * SCREEN_HEIGHT / 4 - 0.05 * SCREEN_HEIGHT / 4, SCREEN_HEIGHT / 2 - y1 * SCREEN_HEIGHT / 4 - 0.05 * SCREEN_HEIGHT / 4, static_cast<int>(0.1 * SCREEN_HEIGHT / 4), static_cast<int>(0.1 * SCREEN_HEIGHT / 4)};
+    SDL_RenderFillRect(renderer, &outlineRect);
+    SDL_RenderDrawLine(renderer, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, static_cast<int>(SCREEN_WIDTH / 2 + x1 * SCREEN_HEIGHT / 4), static_cast<int>(SCREEN_HEIGHT / 2 - y1 * SCREEN_HEIGHT / 4));
+    SDL_RenderDrawLine(renderer, static_cast<int>(SCREEN_WIDTH / 2 + x1 * SCREEN_HEIGHT / 4), static_cast<int>(SCREEN_HEIGHT / 2 - y1 * SCREEN_HEIGHT / 4), static_cast<int>(SCREEN_WIDTH / 2 + x2 * SCREEN_HEIGHT / 4), static_cast<int>(SCREEN_HEIGHT / 2 - y2 * SCREEN_HEIGHT / 4));
+
+    return true;
+}
+
+bool draw_goal(double x, double y)
+{
+    SDL_Rect outlineRect = {static_cast<int>(SCREEN_WIDTH / 2 + x * SCREEN_WIDTH / 4 - 0.05 * SCREEN_HEIGHT / 4), static_cast<int>(SCREEN_HEIGHT / 2 - y * SCREEN_HEIGHT / 4 - 0.05 * SCREEN_HEIGHT / 4), static_cast<int>(0.1 * SCREEN_HEIGHT / 4), static_cast<int>(0.1 * SCREEN_HEIGHT / 4)};
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+    SDL_RenderFillRect(renderer, &outlineRect);
+
+    return true;
+}
+
+void sdl_clean()
+{
+    // Destroy renderer
+    if (renderer)
+        SDL_DestroyRenderer(renderer);
+    //Destroy window
+    if (window)
+        SDL_DestroyWindow(window);
+    //Quit SDL
+    SDL_Quit();
+}
+#endif
+
+struct Params {
+    struct blackdrops {
+        BO_PARAM(size_t, action_dim, 2);
+        BO_PARAM(size_t, model_input_dim, 6);
+        BO_PARAM(size_t, model_pred_dim, 4);
+        BO_PARAM(double, dt, 0.1);
+        BO_PARAM(double, T, 4.0);
+        BO_DYN_PARAM(bool, verbose);
+        BO_DYN_PARAM(double, boundary);
+    };
+
+    struct gp_model {
+        BO_PARAM(double, noise, 0.01);
+    };
+
+    struct mean_constant {
+        BO_PARAM(double, constant, 0.0);
+    };
+
+    struct kernel : public limbo::defaults::kernel {
+        BO_PARAM(double, noise, gp_model::noise());
+        BO_PARAM(bool, optimize_noise, true);
+    };
+
+    struct kernel_squared_exp_ard : public limbo::defaults::kernel_squared_exp_ard {
+    };
+
+    struct opt_cmaes : public limbo::defaults::opt_cmaes {
+        BO_DYN_PARAM(double, max_fun_evals);
+        BO_DYN_PARAM(double, fun_tolerance);
+        BO_DYN_PARAM(int, restarts);
+        BO_DYN_PARAM(int, elitism);
+        BO_DYN_PARAM(bool, handle_uncertainty);
+
+        BO_PARAM(int, variant, aBIPOP_CMAES);
+        BO_PARAM(int, verbose, false);
+        BO_PARAM(bool, fun_compute_initial, true);
+        // BO_PARAM(double, fun_target, 30);
+        BO_DYN_PARAM(double, ubound);
+        BO_DYN_PARAM(double, lbound);
+    };
+
+    struct opt_rprop : public limbo::defaults::opt_rprop {
+        BO_PARAM(int, iterations, 300);
+        BO_PARAM(double, eps_stop, 1e-4);
+    };
+
+    struct opt_parallelrepeater : public limbo::defaults::opt_parallelrepeater {
+        BO_PARAM(int, repeats, 3);
+    };
+};
+
+struct PolicyParams {
+    struct blackdrops : public Params::blackdrops {
+    };
+
+    struct nn_policy {
+        BO_PARAM(size_t, state_dim, Params::blackdrops::model_input_dim());
+        BO_PARAM(size_t, action_dim, Params::blackdrops::action_dim());
+        BO_PARAM_ARRAY(double, max_u, 2., 2.);
+        BO_DYN_PARAM(int, hidden_neurons);
+        BO_PARAM_ARRAY(double, limits, 10., 10., 1., 1., 1., 1.);
+        BO_PARAM(double, af, 1.0);
+    };
+};
+
+Eigen::VectorXd tip(double theta1, double theta2)
+{
+    double l = 0.5;
+
+    double y = l*(std::cos(theta1)+std::cos(theta2));
+    double x = -l*(std::sin(theta1)+std::sin(theta2));
+
+    Eigen::VectorXd ret(2);
+    ret << x, y;
+
+    return ret;
+}
+
+struct Pendulum : public blackdrops::system::ODESystem<Params> {
+    typedef std::vector<double> ode_state_type;
+
+    Eigen::VectorXd init_state() const
+    {
+        Eigen::VectorXd init = Eigen::VectorXd::Zero(4);
+        init(2) = M_PI;
+        init(3) = M_PI;
+
+        return init;
+    }
+
+    Eigen::VectorXd transform_state(const Eigen::VectorXd& original_state) const
+    {
+        Eigen::VectorXd trans_state = Eigen::VectorXd::Zero(6);
+        trans_state.head(2) = original_state.head(2);
+        trans_state(2) = std::cos(original_state(2));
+        trans_state(3) = std::sin(original_state(2));
+        trans_state(4) = std::cos(original_state(3));
+        trans_state(5) = std::sin(original_state(3));
+
+        return trans_state;
+    }
+
+    void draw_single(const Eigen::VectorXd& state) const
+    {
+#if defined(USE_SDL) && !defined(NODSP)
+        double dt = Params::blackdrops::dt();
+        //Clear screen
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(renderer);
+
+        draw_pendulum(state(2), state(3));
+        draw_goal(0, 1);
+
+        //Update screen
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(dt * 1000);
+#endif
+    }
+
+    /* The rhs of x' = f(x) */
+    void dynamics(const ode_state_type& x, ode_state_type& dx, double t, const Eigen::VectorXd& u) const
+    {
+        double m1 = 0.5; // [kg]     mass of 1st link
+        double m2 = 0.5; // [kg]     mass of 2nd link
+        double b1 = 0.1; // [Ns/m]   coefficient of friction (1st joint)
+        double b2 = 0.1; // [Ns/m]   coefficient of friction (2nd joint)
+        double l1 = 0.5; // [m]      length of 1st pendulum
+        double l2 = 0.5; // [m]      length of 2nd pendulum
+        double g = 9.82; // [m/s^2]  acceleration of gravity
+        double I1 = m1 * l1 * l1 / 12.; // moment of inertia around pendulum midpoint (1st link)
+        double I2 = m2 * l2 * l2 / 12.; // moment of inertia around pendulum midpoint (2nd link)
+        double u1 = u(0);
+        double u2 = u(1);
+
+        Eigen::MatrixXd A(2, 2);
+        A << l1 * l1 * (0.25 * m1 + m2) + I1, 0.5 * m2 * l1 * l2 * std::cos(x[2] - x[3]),
+            0.5 * m2 * l1 * l2 * std::cos(x[2] - x[3]), l2 * l2 * 0.25 * m2 + I2;
+        Eigen::VectorXd b(2);
+        b << g * l1 * std::sin(x[2]) * (0.5 * m1 + m2) - 0.5 * m2 * l1 * l2 * x[1] * x[1] * std::sin(x[2] - x[3]) + u1 - b1 * x[0],
+            0.5 * m2 * l2 * (l1 * x[0] * x[0] * std::sin(x[2] - x[3]) + g * std::sin(x[3])) + u2 - b2 * x[1];
+        Eigen::VectorXd sol = A.llt().solve(b);
+
+        dx[0] = sol(0);
+        dx[1] = sol(1);
+        dx[2] = x[0];
+        dx[3] = x[1];
+    }
+};
+
+struct RewardFunction {
+    double operator()(const Eigen::VectorXd& from_state, const Eigen::VectorXd& action, const Eigen::VectorXd& to_state) const
+    {
+        double s_c_sq = 0.5 * 0.5;
+
+        Eigen::VectorXd tip_pos = tip(to_state(2), to_state(3));
+        Eigen::VectorXd tip_goal(2);
+        tip_goal << 0.0, 1.0;
+        double derr = (tip_goal - tip_pos).squaredNorm();
+
+        return std::exp(-0.5 / s_c_sq * derr);
+
+
+        //$1-\exp(-0.5*d^2*a)$,  where $a>0$ and  $d^2$ is the squared difference
+        // between the actual and desired position of the tip of the outer pendulum.
+    }
+};
+
+BO_DECLARE_DYN_PARAM(int, PolicyParams::nn_policy, hidden_neurons);
+BO_DECLARE_DYN_PARAM(bool, Params::blackdrops, verbose);
+BO_DECLARE_DYN_PARAM(double, Params::blackdrops, boundary);
+
+BO_DECLARE_DYN_PARAM(double, Params::opt_cmaes, max_fun_evals);
+BO_DECLARE_DYN_PARAM(double, Params::opt_cmaes, fun_tolerance);
+BO_DECLARE_DYN_PARAM(double, Params::opt_cmaes, lbound);
+BO_DECLARE_DYN_PARAM(double, Params::opt_cmaes, ubound);
+BO_DECLARE_DYN_PARAM(int, Params::opt_cmaes, restarts);
+BO_DECLARE_DYN_PARAM(int, Params::opt_cmaes, elitism);
+BO_DECLARE_DYN_PARAM(bool, Params::opt_cmaes, handle_uncertainty);
+
+int main(int argc, char** argv)
+{
+    bool uncertainty = false;
+    int threads = tbb::task_scheduler_init::automatic;
+    bool verbose = false;
+    namespace po = boost::program_options;
+    po::options_description desc("Command line arguments");
+    // clang-format off
+    desc.add_options()("help,h", "Prints this help message")
+                      ("hidden_neurons,n", po::value<int>(), "Number of hidden neurons in NN policy.")
+                      ("boundary,b", po::value<double>(), "Boundary of the values during the optimization.")
+                      ("max_evals,m", po::value<int>(), "Max function evaluations to optimize the policy.")
+                      ("tolerance,t", po::value<double>(), "Maximum tolerance to continue optimizing the function.")
+                      ("restarts,r", po::value<int>(), "Max number of restarts to use during optimization.")
+                      ("elitism,e", po::value<int>(), "Elitism mode to use [0 to 3].")
+                      ("uncertainty,u", po::bool_switch(&uncertainty)->default_value(false), "Enable uncertainty handling.")
+                      ("threads,d", po::value<int>(), "Max number of threads used by TBB")
+                      ("verbose,v", po::bool_switch(&verbose)->default_value(false), "Enable verbose mode.");
+    // clang-format on
+
+    try {
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        if (vm.count("help")) {
+            std::cout << desc << std::endl;
+            return 0;
+        }
+
+        po::notify(vm);
+
+        if (vm.count("threads"))
+            threads = vm["threads"].as<int>();
+        if (vm.count("hidden_neurons")) {
+            int c = vm["hidden_neurons"].as<int>();
+            if (c < 1)
+                c = 1;
+            PolicyParams::nn_policy::set_hidden_neurons(c);
+        }
+        else {
+            PolicyParams::nn_policy::set_hidden_neurons(5);
+        }
+        if (vm.count("boundary")) {
+            double c = vm["boundary"].as<double>();
+            if (c < 0)
+                c = 0;
+            Params::blackdrops::set_boundary(c);
+            Params::opt_cmaes::set_lbound(-c);
+            Params::opt_cmaes::set_ubound(c);
+        }
+        else {
+            Params::blackdrops::set_boundary(0);
+            Params::opt_cmaes::set_lbound(-6);
+            Params::opt_cmaes::set_ubound(6);
+        }
+
+        // Cmaes parameters
+        if (vm.count("max_evals")) {
+            int c = vm["max_evals"].as<int>();
+            Params::opt_cmaes::set_max_fun_evals(c);
+        }
+        else {
+            Params::opt_cmaes::set_max_fun_evals(10000);
+        }
+        if (vm.count("tolerance")) {
+            double c = vm["tolerance"].as<double>();
+            if (c < 0.1)
+                c = 0.1;
+            Params::opt_cmaes::set_fun_tolerance(c);
+        }
+        else {
+            Params::opt_cmaes::set_fun_tolerance(1);
+        }
+        if (vm.count("restarts")) {
+            int c = vm["restarts"].as<int>();
+            if (c < 1)
+                c = 1;
+            Params::opt_cmaes::set_restarts(c);
+        }
+        else {
+            Params::opt_cmaes::set_restarts(3);
+        }
+        if (vm.count("elitism")) {
+            int c = vm["elitism"].as<int>();
+            if (c < 0 || c > 3)
+                c = 0;
+            Params::opt_cmaes::set_elitism(c);
+        }
+        else {
+            Params::opt_cmaes::set_elitism(0);
+        }
+    }
+    catch (po::error& e) {
+        std::cerr << "[Exception caught while parsing command line arguments]: " << e.what() << std::endl;
+        return 1;
+    }
+#if defined(USE_SDL) && !defined(NODSP)
+    //Initialize
+    if (!sdl_init()) {
+        return 1;
+    }
+#endif
+
+#ifdef USE_TBB
+    static tbb::task_scheduler_init init(threads);
+#endif
+
+    Params::blackdrops::set_verbose(verbose);
+    Params::opt_cmaes::set_handle_uncertainty(uncertainty);
+
+    std::cout << std::endl;
+    std::cout << "Cmaes parameters:" << std::endl;
+    std::cout << "  max_fun_evals = " << Params::opt_cmaes::max_fun_evals() << std::endl;
+    std::cout << "  fun_tolerance = " << Params::opt_cmaes::fun_tolerance() << std::endl;
+    std::cout << "  restarts = " << Params::opt_cmaes::restarts() << std::endl;
+    std::cout << "  elitism = " << Params::opt_cmaes::elitism() << std::endl;
+    std::cout << "  handle_uncertainty = " << Params::opt_cmaes::handle_uncertainty() << std::endl;
+    std::cout << "  boundary = " << Params::blackdrops::boundary() << std::endl;
+    std::cout << std::endl;
+
+    using kernel_t = limbo::kernel::SquaredExpARD<Params>;
+    using mean_t = limbo::mean::Constant<Params>;
+    using GP_t = blackdrops::model::MultiGP<Params, limbo::model::GP, kernel_t, mean_t, blackdrops::model::multi_gp::MultiGPParallelLFOpt<Params, limbo::model::gp::KernelLFOpt<Params>>>;
+
+    using policy_opt_t = limbo::opt::Cmaes<Params>;
+
+    using MGP_t = blackdrops::GPModel<Params, GP_t>;
+
+    blackdrops::BlackDROPS<Params, MGP_t, Pendulum, blackdrops::policy::NNPolicy<PolicyParams>, policy_opt_t, RewardFunction> pend_system;
+
+    pend_system.learn(1, 25);
+
+#if defined(USE_SDL) && !defined(NODSP)
+    sdl_clean();
+#endif
+
+    return 0;
+}
