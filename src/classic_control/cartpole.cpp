1fb8f61dc0247c9cb7d129e52a42af4043e7534a
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2017-11-29 15:30:22 +0100

    Bigger pop in CMA-ES

--- src/classic_control/cartpole.cpp
+++ src/classic_control/cartpole.cpp
@@ -210,6 +210,8 @@ struct Params {
         BO_DYN_PARAM(int, elitism);
         BO_DYN_PARAM(bool, handle_uncertainty);
 
+        BO_PARAM(int, lambda, 16);
+
 #ifdef SIMPLE_CMAES
         BO_PARAM(int, variant, aIPOP_CMAES);
 #else
