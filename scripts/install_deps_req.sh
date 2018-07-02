a28dcb73e270c1510dc62002b3824266993f873f
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2018-07-02 12:40:27 +0200

    Fix some issues of #4

--- scripts/install_deps_req.sh
+++ scripts/install_deps_req.sh
@@ -5,13 +5,13 @@ echo "Detected OS: $OS"
 
 if [ $OS = "Darwin" ]; then
     echo "Installing for OSX via brew"
-    brew install cmake eigen3 boost
+    brew install cmake eigen boost tbb
     brew install autoconf automake
 else
     echo "Installing for Ubuntun via apt-get"
     sudo apt-get -qq update
     # install Eigen 3, Boost
-    sudo apt-get --yes --force-yes install cmake libeigen3-dev libboost-serialization-dev libboost-filesystem-dev libboost-test-dev libboost-program-options-dev libboost-thread-dev libboost-regex-dev
+    sudo apt-get --yes --force-yes install cmake libeigen3-dev libtbb-dev libboost-serialization-dev libboost-filesystem-dev libboost-test-dev libboost-program-options-dev libboost-thread-dev libboost-regex-dev
     # install google tests for libcmaes
     sudo apt-get --yes --force-yes install libgtest-dev autoconf automake libtool libgoogle-glog-dev libgflags-dev
 fi
