ce8320bffac9b8dfa5440df754c2a46f0d464cd2
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2018-09-28 18:13:39 +0200

    make the install_deps_alls work on osx (except for robot_dart, because we need the right branch

--- scripts/install_deps_all.sh
+++ scripts/install_deps_all.sh
@@ -1,39 +1,56 @@
 #!/bin/bash
+set -x
 
 OS=$(uname)
 echo "Detected OS: $OS"
 
 if [ $OS = "Darwin" ]; then
-    echo "ERROR: OSX can only be used with install_deps_req.sh"
-    exit 1
+    echo "Installing for OSX via brew"
+    # general stuffs
+    brew install cmake eigen boost tbb autoconf automake
+else
+    # check if we have Ubuntu or not
+    distro_str="$(cat /etc/*-release | grep -s DISTRIB_ID)"
+    distro=$(echo $distro_str | cut -f2 -d'=')
+
+    if [ "$distro" != "Ubuntu" ]; then
+        echo "ERROR: We need an Ubuntu system to use this script"
+        exit 1
+    fi
+
+    # get ubuntu version
+    version_str="$(cat /etc/*-release | grep -s DISTRIB_RELEASE)"
+    version=$(echo $version_str | cut -f2 -d'=')
+    major_version=$(echo $version | cut -f1 -d'.')
+    minor_version=$(echo $version | cut -f2 -d'.')
+
+    # if less than 14.04, exit
+    if [ "$(($major_version))" -lt "14" ]; then
+        echo "ERROR: We need Ubuntu >= 14.04 for this script to work"
+        exit 1
+    fi
+
+    sudo apt-get -qq update
+    # install Eigen 3, Boost and TBB
+    sudo apt-get --yes --force-yes install cmake libeigen3-dev libtbb-dev libboost-serialization-dev libboost-filesystem-dev libboost-test-dev libboost-program-options-dev libboost-thread-dev libboost-regex-dev libsdl2-dev
+    # install google tests for libcmaes
+    sudo apt-get --yes --force-yes install libgtest-dev autoconf automake libtool libgoogle-glog-dev libgflags-dev
 fi
-
-# check if we have Ubuntu or not
-distro_str="$(cat /etc/*-release | grep -s DISTRIB_ID)"
-distro=$(echo $distro_str | cut -f2 -d'=')
-
-if [ "$distro" != "Ubuntu" ]; then
-    echo "ERROR: We need an Ubuntu system to use this script"
-    exit 1
-fi
-
-sudo apt-get -qq update
-# install Eigen 3, Boost and TBB
-sudo apt-get --yes --force-yes install cmake libeigen3-dev libtbb-dev libboost-serialization-dev libboost-filesystem-dev libboost-test-dev libboost-program-options-dev libboost-thread-dev libboost-regex-dev libsdl2-dev
-# install google tests for libcmaes
-sudo apt-get --yes --force-yes install libgtest-dev autoconf automake libtool libgoogle-glog-dev libgflags-dev
 
 # save current directory
 cwd=$(pwd)
 # create install dir
 mkdir -p install
 
-# do libgtest fix for libcmaes
-cd /usr/src/gtest
-sudo mkdir -p build && cd build
-sudo cmake ..
-sudo make
-sudo cp *.a /usr/lib
+# do libgtest fix for libcmaes (linux only)
+if [ $OS = "Linux" ]; then
+    cd /usr/src/gtest
+    sudo mkdir -p build && cd build
+    sudo cmake ..
+    sudo make
+    sudo cp *.a /usr/lib
+fi
+
 # install libcmaes
 cd ${cwd}/deps/libcmaes
 mkdir -p build && cd build
@@ -55,28 +72,28 @@ make install
 # go back to original directory
 cd ../..
 
-# get ubuntu version
-version_str="$(cat /etc/*-release | grep -s DISTRIB_RELEASE)"
-version=$(echo $version_str | cut -f2 -d'=')
-major_version=$(echo $version | cut -f1 -d'.')
-minor_version=$(echo $version | cut -f2 -d'.')
+if [ $OS = "Darwin" ]; then
+ # for DART
+    brew install assimp
+    brew install homebrew/science/libccd
+    brew install dartsim/dart/fcl
+    brew install open-scene-graph
 
-# if less than 14.04, exit
-if [ "$(($major_version))" -lt "14" ]; then
-    echo "ERROR: We need Ubuntu >= 14.04 for this script to work"
-    exit 1
+    brew install tinyxml
+    brew install tinyxml2
+    brew install ros/deps/urdfdom
+else
+    # if we have less than 16.04, we need some extra stuff
+    if [ "$(($major_version))" -lt "16" ]; then
+        sudo apt-add-repository ppa:libccd-debs/ppa -y
+        sudo apt-add-repository ppa:fcl-debs/ppa -y
+    fi
+    sudo apt-add-repository ppa:dartsim/ppa -y
+    sudo apt-get -qq update
+    sudo apt-get --yes --force-yes install build-essential pkg-config libassimp-dev libccd-dev libfcl-dev
+    sudo apt-get --yes --force-yes install libnlopt-dev libbullet-dev libtinyxml-dev libtinyxml2-dev liburdfdom-dev liburdfdom-headers-dev libxi-dev libxmu-dev freeglut3-dev libopenscenegraph-dev   
 fi
 
-# install DART dependencies
-# if we have less than 16.04, we need some extra stuff
-if [ "$(($major_version))" -lt "16" ]; then
-    sudo apt-add-repository ppa:libccd-debs/ppa -y
-    sudo apt-add-repository ppa:fcl-debs/ppa -y
-fi
-sudo apt-add-repository ppa:dartsim/ppa -y
-sudo apt-get -qq update
-sudo apt-get --yes --force-yes install build-essential pkg-config libassimp-dev libccd-dev libfcl-dev
-sudo apt-get --yes --force-yes install libnlopt-dev libbullet-dev libtinyxml-dev libtinyxml2-dev liburdfdom-dev liburdfdom-headers-dev libxi-dev libxmu-dev freeglut3-dev libopenscenegraph-dev
 # install DART
 cd deps/dart
 mkdir -p build && cd build
@@ -87,7 +104,11 @@ make install
 cd ../../..
 
 # just as fail-safe
-sudo ldconfig
+if [ $OS = "Darwin" ]; then
+    update_dyld_shared_cache
+else
+    sudo ldconfig
+fi
 
 # configure paths to find DART related libraries properly
 source ./scripts/paths.sh
